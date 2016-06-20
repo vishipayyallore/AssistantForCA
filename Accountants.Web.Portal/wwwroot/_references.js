348439150139faad530aaeb4d303072dff67069c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-06-21 00:05:22 +0530

    AAA

--- Accountants.Web.Portal/wwwroot/_references.js
+++ Accountants.Web.Portal/wwwroot/_references.js
@@ -4,5 +4,4 @@
 /// <reference path="app/Config.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
-/// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
